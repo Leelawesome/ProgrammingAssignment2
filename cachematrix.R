6e6237b3034f319de6a6d9c1328920ffff7b1738
Leela <leela.papadioti@gmail.com>
2014-04-28 04:39:44 +0300

    cachematrix assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function makeCacheMatrix creates a special "matrix" object that can cache its inverse.
+## It is really a list containing a function to:
+## 1. Set the value of the matrix
+## 2. Get the value of the matrix
+## 3. Set the value of the inverse matrix
+## 4. Get the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    ## Caches the inverse matrix.
+    inversematrix <- NULL
+    
+    ## Sets the matrix.
+    set <- function(y) {
+        x <<- y
+        inversematrix <<- NULL
+    }
+    
+    ## Returns the matrix.
+    get <- function() {
+        x
+    }
+    
+    ## Sets the inverse matrix.
+    setinverse <- function(inverse){
+        inversematrix <<- inverse
+    }
+    
+    ## Returns the cached inverse matrix.
+    getinverse <- function() {
+        inversematrix
+    }
+    
+    ## Returns our special "matrix".
+    list(set = set, 
+         get = get, 
+         setinverse = setinverse, 
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inverse of the special "matrix". 
+## First it checks if the inverse matrix has already been calculated.
+## If so, it retrieves the inverse matrix from the cache, it displays a message, and skips calculation.
+## Otherwise, it calculates the inverse matrix and stores it in the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getinverse()
+    
+    if (!is.null(inverse)) {
+        
+        ## The inverse matrix is in the cache, so it retrieves it and displays a message.
+        message("getting cached data")
+        return(inverse)
+    }
+    
+    ## The inverse matrix is not yet calculated, so it calculates and stores it in the cache.
+    data <- x$get()
+    inverse <- solve(data)
+    x$setinverse(inverse)
+    
+    ## Returns a matrix that is the inverse of 'x'
+    inverse
 }
